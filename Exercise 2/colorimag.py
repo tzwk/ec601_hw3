a51b7f0bd9a1bbf95b97d87480b13bb7e37d6eaa
tzwk <31743144+tzwk@users.noreply.github.com>
2017-12-15 20:21:14 -0500

    Update colorimag.py

--- Exercise 2/colorimag.py
+++ Exercise 2/colorimag.py
@@ -1,13 +1,11 @@
 import sys
 import cv2
 
-
-def show_and_save_planes(image, labels):
-    planes = cv2.split(image)
-    for plane, label in zip(planes, labels):
-        cv2.imshow(label, plane)
-        cv2.imwrite(label + ".png", plane)
-
+def showandsave(imag, names):
+    planes = cv2.split(imag)
+    for plane, name in zip(planes, names):
+        cv2.imshow(name, plane)
+        cv2.imwrite(name + ".png", plane)
 
 if __name__ == "__main__":
 	rgb_image = cv2.imread(sys.argv[1], 1)
@@ -20,14 +18,10 @@ if __name__ == "__main__":
 	for plane, label in zip((blue, green, red), ("Blue", "Green", "Red")):
     	cv2.imshow(label, plane)
     	cv2.imwrite(label + ".png", plane)
-
-
 	ycrcb_image = cv2.cvtColor(rgb_image, cv2.COLOR_BGR2YCrCb)
 	print("YCrCb value of pixel (20, 25): " + str(ycrcb_image[20, 25]))
-	show_and_save_planes(ycrcb_image, ("Y", "Cr", "Cb"))
-
+	showandsave(ycrcb_image, ("Y", "Cr", "Cb"))
 	hsv_image = cv2.cvtColor(rgb_image, cv2.COLOR_BGR2HSV)
 	print("HSV value of pixel (20, 25): " + str(hsv_image[20, 25]))
-	show_and_save_planes(hsv_image, ("Hue", "Saturation", "Value"))
-
+	showandsave(hsv_image, ("Hue", "Saturation", "Value"))
 	cv2.waitKey(0)
